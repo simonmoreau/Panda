d22060543c45cef859ecbda19063d48faa253e5e
simonmoreau <simon@bim42.com>
2018-11-13 17:00:19 +0100

    Enable screenshot

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-
+import * as html2canvas from "html2canvas"
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  
-  pdfSrc: string =  '/pdf-test.pdf';
-  
+
+  pdfSrc: string = '/pdf-test.pdf';
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -32,23 +32,29 @@ export class HomeComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
-  onFileInput($event) : void {
+  onFileInput($event): void {
     this.ReadPDF($event.target);
   }
-  
+
   ReadPDF(inputValue: any) {
     let file = inputValue.files[0];
-  
+
     if (typeof (FileReader) !== 'undefined') {
       let reader = new FileReader();
-  
+
       reader.onload = (e: any) => {
         this.pdfSrc = e.target.result;
       };
-  
+
       reader.readAsArrayBuffer(file);
     }
+  }
+
+  screenshot() {
+    html2canvas(document.getElementById('pdf')).then((canvas) => {
+      window.open().document.write('<img src="' + canvas.toDataURL() + '" />');
+    });
   }
 }
